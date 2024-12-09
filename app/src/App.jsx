f80de12c0bc2050d96b1a926e6ece85967da3cbd
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 00:17:38 +0530

    imported Header and Slideslow in App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Slideslow from "./components/Slideslow"
+import {top, bottom} from "./data/slideslow.json"
 
 function App() {
 
   return (
-   <div>
-    App
+   <div className='w-full max-w-screen-sm h-full min-h-screen mx-auto px-5 responsive'>
+
+      <Header />
+
+      <main className="h-screen">
+        <Slideslow data={top}/>
+
+      </main>
+      <Footer />
    </div>
   )
 }
