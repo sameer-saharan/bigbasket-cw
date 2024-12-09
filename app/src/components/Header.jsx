a1f8673bffb37505ac223d966de65d997a884117
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 00:15:56 +0530

    Header intital stage

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex flex-col items-center gap-y-5 shadow-sm p-5 sticky top-0 md:flex-row md:justify-between'>
+      <img src="/BigBasket_Logo.svg" className='w-28'/>
+      <div className='border border-[#A0CD4A] rounded-lg md:w-1/2'>
+        <img src="" alt="" />
+        <input 
+          type="text" 
+          className='p-2 outline-none bg-transparent'
+          placeholder='Search for Products...'
+        />
+      </div>
+      <button className='bg-[#2f2f2f] rounded-md p-2 text-white text-xs font-bold hover:bg-[#000000] hidden md:block'>
+        Login/Signup
+      </button>
+    </div>
   )
 }
 
