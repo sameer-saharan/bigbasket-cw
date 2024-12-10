5c7bce0d245eadb739613ffa32365bf6c330791b
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 18:54:50 +0530

    CategoryButton CSS fix

--- app/src/components/CategoryButton.jsx
+++ app/src/components/CategoryButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CategoryButton({buttonText}) {
   return (
-    <button className='bg-[#749c28] text-[#ffffff] font-mono font-semibold py-2 px-4 rounded-md hover:bg-[#A0CD4A] hover:text-gray-800 hover:shadow-sm hover:shadow-[#4f4d4d] transition-all ease-in-out'>
+    <button className='bg-[#749c28] text-[#ffffff] font-mono font-semibold py-1 px-2 text-xs xl:text-sm 2xl:text-base xl:py-2 xl:px-4 rounded-md hover:bg-[#A0CD4A] hover:text-gray-800 hover:shadow-sm hover:shadow-[#4f4d4d] transition-all ease-in-out'>
         {buttonText}
     </button>
   )
