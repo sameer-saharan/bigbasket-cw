19d8644f36fb7c5eee029e34073fcf5a14c6d3ca
Sameer Saharan <albaadigoru@gmail.com>
2024-12-10 15:43:35 +0530

    added another slideslow at bottom of page (above footer)

---
+++ app/src/components/SecondSlideslow.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react'
+
+function SecondSlideslow({data}) {
+    const photos = data;
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (
+                prevIndex === photos.length - 1 ? 0 : prevIndex + 1
+            ))
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [])
+
+  return (
+    <div className='overflow-hidden px-5'>
+        <div 
+        className='h-[100px] bg-cover bg-center rounded-xl md:h-[200px] lg:h-[300px] 2xl:h-[400px] flex items-center justify-between md:justify-center md:items-end'
+        style={{backgroundImage: `url(${photos[index]})`}}
+        ></div>
+    </div>
+  )
+}
+
+export default SecondSlideslow
