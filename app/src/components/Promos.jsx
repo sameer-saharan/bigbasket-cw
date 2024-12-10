cfdc79ded274cdddc2c1d043ca3a3795fcf0ab5d
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 23:00:10 +0530

    Added unique keys fnc

--- app/src/components/Promos.jsx
+++ app/src/components/Promos.jsx
@@ -5,14 +5,14 @@ import PromoCards from "../components/cards/PromoCards"
 function Promos() {
   return (
     <div className='flex flex-col'>
-      <PromoCards cards={topOffers} title={"Top Offers"} width={"w-60"}/>
-      <PromoCards cards={fruitAndVeg} title={"Fruits and Vegetables"} width={"w-60"}/>
-      <PromoCards cards={yourDailyStaples} title={"Your Daily Staples"} width={"w-40"}/>
-      <PromoCards cards={beverages} title={"Beverages"} width={"w-40"}/>
-      <PromoCards cards={snacksStore} title={"Snacks Store"} width={"w-60"}/>
-      <PromoCards cards={cleaningAndHouse} title={"Cleaning & Household"} width={"w-60"}/>
-      <PromoCards cards={beautyAndHygine} title={"Beauty and Hygiene"} width={"w-40"}/>
-      <PromoCards cards={homeAndKitchen} title={"Home and Kitchen"} width={"w-40"}/>
+      <PromoCards cards={topOffers} title={"Top Offers"} width={"w-60"} unique={"to"}/>
+      <PromoCards cards={fruitAndVeg} title={"Fruits and Vegetables"} width={"w-60"} unique={"fv"}/>
+      <PromoCards cards={yourDailyStaples} title={"Your Daily Staples"} width={"w-40"} unique={"yd"}/>
+      <PromoCards cards={beverages} title={"Beverages"} width={"w-40"} unique={"bv"}/>
+      <PromoCards cards={snacksStore} title={"Snacks Store"} width={"w-60"} unique={"ss"}/>
+      <PromoCards cards={cleaningAndHouse} title={"Cleaning & Household"} width={"w-60"} unique={"ch"}/>
+      <PromoCards cards={beautyAndHygine} title={"Beauty and Hygiene"} width={"w-40"} unique={"bh"}/>
+      <PromoCards cards={homeAndKitchen} title={"Home and Kitchen"} width={"w-40"} unique={"hk"}/>
       
 
     </div>
--- app/src/components/cards/PromoCards.jsx
+++ app/src/components/cards/PromoCards.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function PromoCards({cards, title, width}) {
+function PromoCards({cards, title, width, unique}) {
   return (
     <div className='flex flex-col py-5'>
       <h3 className='text-2xl font-semibold py-3'>{title}</h3>
         <div className='flex flex-wrap justify-center gap-10'>
-            {cards.map((png) => (
-                <img src={png} className={`rounded-lg ${width} shadow-md hover:shadow-2xl cursor-pointer`} />
+            {cards.map((png, index) => (
+                <img key={`${unique}${index}`} src={png} className={`rounded-lg ${width} shadow-md hover:shadow-2xl cursor-pointer`} />
             ))}
         </div>
     </div>
