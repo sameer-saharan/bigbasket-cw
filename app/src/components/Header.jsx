9a5fbb0ea4f1a8c7c9f96e6e9f6ff6f1eb4c6144
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 21:13:43 +0530

    changed some tailwindcss properties

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='flex flex-col items-center gap-y-5 bg-white shadow-sm p-5 sticky top-0 md:flex-row md:justify-between'>
+    <div className='flex mx-auto max-w-screen-sm flex-col items-center gap-y-5 p-5 sticky top-0 md:flex-row md:justify-between responsive'>
       <img src="/BigBasket_Logo.svg" className='w-28'/>
       <div className='border border-[#A0CD4A] rounded-lg md:w-1/2'>
         <img src="" alt="" />
