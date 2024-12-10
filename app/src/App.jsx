b0e4f1ea992cd5e3dab4dceeb55c479c46182847
Sameer Saharan <albaadigoru@gmail.com>
2024-12-10 15:44:17 +0530

    made required changes to use react router effectively

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Slideslow from "./components/Slideslow"
-import {top, bottom} from "./data/slideslow.json"
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
    <div className='w-full max-w-screen-sm h-full min-h-screen mx-auto px-5 responsive'>
-
       <Header />
-
-      <main className="h-screen">
-        <Slideslow data={top}/>
-
-      </main>
+      <Outlet />
       <Footer />
    </div>
   )
