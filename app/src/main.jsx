4e606ef916d9111cff90379a643e74505b11ac37
Sameer Saharan <albaadigoru@gmail.com>
2024-12-10 16:01:16 +0530

    Added React Router and applied routes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,24 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Products from './pages/Products.jsx'
+import {Electronics, Grocery, TeaAndCoffee, Household, Drinks} from "./components/category"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />}/>
+      <Route path='/products' element={<Products />}/> 
+      <Route path='/products/electronics' element={<Electronics />}/>
+      <Route path='/products/grocery' element={<Grocery />}/>
+      <Route path='/products/teacoffee' element={<TeaAndCoffee />}/>
+      <Route path='/products/household' element={<Household />}/>
+      <Route path='/products/drinks' element={<Drinks />}/>
+
+    </Route>
+))
 createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 )
