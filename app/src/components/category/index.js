b4aaefe5a1d50b98c8e170d1efd0ed8619fea58e
Sameer Saharan <albaadigoru@gmail.com>
2024-12-10 17:46:40 +0530

    created CustomProducts component to show selected products on Home

--- app/src/components/category/index.js
+++ app/src/components/category/index.js
@@ -3,5 +3,6 @@ import Electronics from "./Electronics";
 import Grocery from "./Grocery";
 import Household from "./Household";
 import TeaAndCoffee from "./TeaAndCoffee";
+import CustomProducts from "./CustomProducts";
 
-export {Drinks, Electronics, Grocery, Household, TeaAndCoffee}
+export {Drinks, Electronics, Grocery, Household, TeaAndCoffee, CustomProducts}
