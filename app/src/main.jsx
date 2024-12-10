2fa3526136b0deae0f715e1e20c3655cd16334f8
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 23:51:48 +0530

    removed routes for categories

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,19 +4,12 @@ import App from './App.jsx'
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
 import Home from './pages/Home.jsx'
 import Products from './pages/Products.jsx'
-import {Electronics, Grocery, TeaAndCoffee, Household, Drinks} from "./components/category"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path='' element={<Home />}/>
       <Route path='/products' element={<Products />}/> 
-      <Route path='/products/electronics' element={<Electronics />}/>
-      <Route path='/products/grocery' element={<Grocery />}/>
-      <Route path='/products/teacoffee' element={<TeaAndCoffee />}/>
-      <Route path='/products/household' element={<Household />}/>
-      <Route path='/products/drinks' element={<Drinks />}/>
-
     </Route>
 ))
 createRoot(document.getElementById('root')).render(
