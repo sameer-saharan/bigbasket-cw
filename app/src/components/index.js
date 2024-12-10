eb1f6f0ac908bc66150da2a05c11d1d8350a0552
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 20:55:52 +0530

    exported Header and Footer from single file and then imported to App.jsx

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -4,5 +4,7 @@ import CategoryButton from "./CategoryButton";
 import Dropdown from "./Dropdown";
 import Promos from "./Promos";
 import About from "./About";
+import Header from "./Header";
+import Footer from "./Footer";
 
-export {Slideslow, SecondSlideslow, CategoryButton, Dropdown, Promos, About}
+export {Slideslow, SecondSlideslow, CategoryButton, Dropdown, Promos, About, Header, Footer}
