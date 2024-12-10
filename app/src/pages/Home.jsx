f798a729f165f4c9f06e99801b88a9f1d895a9b9
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 18:56:35 +0530

    Imported Dropdown in Home and adjusted CSS accordingly

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import {top, bottom} from "../data/slideslow.json"
 import {CustomProducts} from "../components/category"
 import { Link } from "react-router-dom"
 import CategoryButton from "../components/CategoryButton"
+import Dropdown from "../components/Dropdown"
 
 function Home() {
 
@@ -13,8 +14,9 @@ function Home() {
 
         <div className="py-20">
           <div>
-            <div className="flex justify-between px-5 py-4 outline outline-gray-200 outline-1 rounded-t-md">
-              <div className="flex items-center gap-x-5">
+            <div className="flex justify-between items-center px-5 py-4 outline outline-gray-200 outline-1 rounded-t-md">
+              <Dropdown />
+              <div className="hidden lg:flex items-center gap-x-1 lg:gap-x-2 xl:gap-x-5">
                 <span className="font-bold">Shop by Categories:</span>
                 <Link to={"/products/electronics"}>
                   <CategoryButton buttonText={"Electronics"}/>
@@ -23,18 +25,18 @@ function Home() {
                   <CategoryButton buttonText={"Grocery"}/>
                 </Link>
                 <Link to={"/products/teacoffee"}>
-                  <CategoryButton buttonText={"Tea & Coffee"}/>
+                  <CategoryButton buttonText={"Beverages"}/>
                 </Link>
                 <Link to={"/products/household"}>
                   <CategoryButton buttonText={"Household"}/>
                 </Link>
                 <Link to={"/products/drinks"}>
-                  <CategoryButton buttonText={"Beverages"}/>
+                  <CategoryButton buttonText={"Drinks"}/>
                 </Link>
               </div>
               <div className="flex items-center">
                 <Link to={"/products"}>
-                <span className="text-xl text-[#494848] underline font-bold hover:text-black transition-all ease-in-out">View All Products</span>
+                <span className="text-xs xl:text-lg text-[#494848] underline font-bold hover:text-black transition-all ease-in-out">View All Products</span>
                 </Link>
               </div>
             </div>
