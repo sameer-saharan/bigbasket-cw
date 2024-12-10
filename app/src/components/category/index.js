d17c402a3e41ea5bad60ca1c18ad77da01a4b102
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-10 15:45:41 +0530

    single .js file to export all category components

---
+++ app/src/components/category/index.js
@@ -0,0 +1,7 @@
+import Drinks from "./Drinks";
+import Electronics from "./Electronics";
+import Grocery from "./Grocery";
+import Household from "./Household";
+import TeaAndCoffee from "./TeaAndCoffee";
+
+export {Drinks, Electronics, Grocery, Household, TeaAndCoffee}
