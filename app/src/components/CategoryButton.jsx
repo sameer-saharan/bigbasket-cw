8f4a44727d71b860e1a7455bf1612594284d2db7
Sameer Saharan <albaadigoru@gmail.com>
2024-12-10 23:53:27 +0530

    clickHandler prop config

--- app/src/components/CategoryButton.jsx
+++ app/src/components/CategoryButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function CategoryButton({buttonText}) {
+function CategoryButton({buttonText, clickHandler}) {
   return (
-    <button className='bg-[#749c28] text-[#ffffff] font-mono font-semibold py-1 px-2 text-xs xl:text-sm 2xl:text-base xl:py-2 xl:px-4 rounded-md hover:bg-[#A0CD4A] hover:text-gray-800 hover:shadow-sm hover:shadow-[#4f4d4d] transition-all ease-in-out'>
+    <button onClick={clickHandler}
+    className='bg-[#749c28] text-[#ffffff] font-mono font-semibold py-1 px-2 text-xs xl:text-sm 2xl:text-base xl:py-2 xl:px-4 rounded-md hover:bg-[#A0CD4A] hover:text-gray-800 hover:shadow-sm hover:shadow-[#4f4d4d] transition-all ease-in-out'>
         {buttonText}
     </button>
   )
