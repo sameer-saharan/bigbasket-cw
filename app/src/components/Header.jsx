47c2875597a049db2cb4484f34a3235dcc48a7d4
Sameer Saharan <albaadigoru@gmail.com>
2024-12-10 23:16:06 +0530

    Added: Cross & Search icon in the Header's bar

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 
 function Header() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className='flex mx-auto max-w-screen-sm flex-col items-center gap-y-5 p-5 sticky top-0 md:flex-row md:justify-between responsive'>
       <img src="/BigBasket_Logo.svg" className='w-28'/>
-      <div className='border border-[#A0CD4A] rounded-lg md:w-1/2'>
-        <img src="" alt="" />
-        <input 
-          type="text" 
-          className='p-2 outline-none bg-transparent'
-          placeholder='Search for Products...'
-        />
+      <div className='flex justify-between px-2 border border-[#A0CD4A] rounded-lg md:w-1/2'>
+        <div className='flex items-center'>
+          <img src="/search-alt-1-svgrepo-com.svg" className='w-5'/>
+          <input 
+            type="text" 
+            value={inputText}
+            className='p-2 outline-none bg-transparent'
+            placeholder='Search for Products...'
+            onChange={(e) => setInputText(e.target.value)}
+          />
+        </div>
+        <button 
+        onClick={() => setInputText("")}
+        className={`cursor-pointer ${inputText ? '' : 'hidden'}`}
+        >
+          <span className="px-2" style={{backgroundImage: `url('/cross-circle-svgrepo-com.svg')`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', }}></span>
+        </button>
+        
       </div>
       <button className='bg-[#2f2f2f] rounded-md p-2 text-white text-xs font-bold hover:bg-[#000000] hidden md:block'>
         Login/Signup
