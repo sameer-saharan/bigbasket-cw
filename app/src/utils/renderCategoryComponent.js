061227c5dadc4259037d77ff5559b2584e23dba0
Sameer Saharan <albaadigoru@gmail.com>
2024-12-11 10:47:29 +0530

    added util for category wise render

---
+++ app/src/utils/renderCategoryComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {CustomProducts, Drinks, Electronics, Grocery, Household, TeaAndCoffee} from "../components/category"
+
+export const renderCategoryComponent = (selectedCategory) => {
+    switch (selectedCategory) {
+      case "Electronics":
+        return Electronics ;
+      case "Grocery":
+        return Grocery ;
+      case "Beverages":
+        return TeaAndCoffee ;
+      case "Household":
+        return Household ;
+      case "Drinks":
+        return Drinks ;
+      default:
+        return CustomProducts;
+    }
+};
