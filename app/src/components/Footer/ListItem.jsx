fff27b2867cf5704117ca577528574e9e07989cd
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2024-12-11 18:06:38 +0530

    Added hover text effect on Footer items

--- app/src/components/Footer/ListItem.jsx
+++ app/src/components/Footer/ListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ListItem({text}) {
   return (
-    <li className='text-sm py-1 text-[#e2dede] cursor-pointer hover:underline hover:font-semibold'>{text}</li>
+    <li className='text-sm py-1 text-[#e2dede] cursor-pointer hover:underline hover:font-semibold hover:text-[#A0CD4A]'>{text}</li>
   )
 }
 
