9097be4afec8f6706689e7d94350111455751169
Sameer Saharan <albaadigoru@gmail.com>
2024-12-11 13:06:26 +0530

    added social svg's in Social.jsx

---
+++ app/src/components/Footer/Socials.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+
+function Socials() {
+  return (
+    <div className="flex lg:gap-x-4 xl:gap-x-9 mb-6">
+      <a
+        href="https://www.facebook.com/pages/Bigbasketcom/139311472851666"
+        data-button="facebook"
+        className="group"
+      >
+        <div className="absolute opacity-0 group-hover:opacity-100" >
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 4a4 4 0 0 1 4-4h16a4 4 0 0 1 4 4v16a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4Z"
+              fill="#fff"
+            ></path>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.112 12.007H7V8.674h3.112V7a5 5 0 0 1 5-5h1.666v3.333h-1.666c-.92 0-1.667.747-1.667 1.667v1.674h3.304v3.333h-3.304V22h-3.333v-9.993Z"
+              fill="#000"
+            ></path>
+          </svg>
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-0" >
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.112 12.007H7V8.674h3.112V7a5 5 0 0 1 5-5h1.666v3.333h-1.666c-.92 0-1.667.747-1.667 1.667v1.674h3.304v3.333h-3.304V22h-3.333v-9.993Z"
+              fill="#DDD"
+            ></path>
+          </svg>
+        </div>
+      </a>
+      <a
+        href="https://www.instagram.com/bigbasketcom/"
+        data-button="instagram"
+        className="group"
+      >
+        <div className="absolute opacity-0 group-hover:opacity-100">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 4a4 4 0 0 1 4-4h16a4 4 0 0 1 4 4v16a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4Z"
+              fill="#fff"
+            ></path>
+            <rect x="2" y="2" width="20" height="20" rx="6" fill="#000"></rect>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M7 12a5 5 0 1 0 10 0 5 5 0 0 0-10 0Zm8.6 0a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM18 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+              fill="#fff"
+            ></path>
+          </svg>
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-0">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M16 2H8a6 6 0 0 0-6 6v8a6 6 0 0 0 6 6h8a6 6 0 0 0 6-6V8a6 6 0 0 0-6-6ZM4 8a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H8a4 4 0 0 1-4-4V8Zm8 9a5 5 0 1 1 0-10 5 5 0 0 1 0 10Zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm6-8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+              fill="#DDD"
+            ></path>
+          </svg>
+        </div>
+      </a>
+      <a
+        href="https://twitter.com/Bigbasket_com"
+        data-button="twitter"
+        className="group"
+      >
+        <div className="absolute opacity-0 group-hover:opacity-100">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 4a4 4 0 0 1 4-4h16a4 4 0 0 1 4 4v16a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4Z"
+              fill="#fff"
+            ></path>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M8.077 20c7.246 0 11.438-5.626 11.438-10.965 0-.147 1.03-1.03 1.47-2.874a.441.441 0 0 0-.616-.512c-.88.404-1.773.084-2.161-.376a3.874 3.874 0 0 0-5.574-.175 4.112 4.112 0 0 0-1.14 3.86c-3.351.24-5.652-1.349-7.544-3.526-.244-.283-.697-.134-.748.24-.28 2.082-.368 7.144 4.648 10.051-.871 1.252-2.576 1.991-4.474 2.308-.42.071-.52.653-.136.841a10.923 10.923 0 0 0 4.837 1.125"
+              fill="#000"
+            ></path>
+            <mask
+              id="twitter-fill_svg__a"
+              mask-type="alpha"
+              maskUnits="userSpaceOnUse"
+              x="3"
+              y="4"
+              width="18"
+              height="17"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M8.077 20c7.246 0 11.438-5.626 11.438-10.965 0-.147 1.03-1.03 1.47-2.874a.441.441 0 0 0-.616-.512c-.88.404-1.773.084-2.161-.376a3.874 3.874 0 0 0-5.574-.175 4.112 4.112 0 0 0-1.14 3.86c-3.351.24-5.652-1.349-7.544-3.526-.244-.283-.697-.134-.748.24-.28 2.082-.368 7.144 4.648 10.051-.871 1.252-2.576 1.991-4.474 2.308-.42.071-.52.653-.136.841a10.923 10.923 0 0 0 4.837 1.125"
+                fill="#fff"
+              ></path>
+            </mask>
+            <g mask="url(#twitter-fill_svg__a)">
+              <path fill="#000" d="M0 0h24v24H0z"></path>
+            </g>
+          </svg>
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-0">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="m21.12 4.508 1.414.165-.636 1.275-.931 1.865a1.475 1.475 0 0 1-.197.397c-.04.063-.177.262-.183.272a2.089 2.089 0 0 0-.082.131v2.389c0 6.111-5.915 9.995-11.501 9.995-1.143 0-2.012-.054-3.006-.271-1.637-.357-2.855-1.067-3.412-2.308l-.574-1.277 1.395-.129c1.261-.116 2.355-.356 3.195-.678-2.306-.37-3.598-1.383-3.598-3.285v-1h1c.22 0 .417-.018.595-.05C2.868 10.963 2 9.303 2 7.001v-.204c-.007-1.16.055-1.78.373-2.571.204-.508.498-.977.894-1.407L4.023 2l.733.838c2.418 2.764 4.808 4.44 7.248 4.643.01-2.55 1.938-4.48 4.5-4.48 1.195 0 2.279.454 3.13 1.272.467.096.962.174 1.485.235Zm-2.211 1.654-.307-.07-.212-.232c-.512-.561-1.154-.858-1.886-.858-1.463 0-2.5 1.042-2.5 2.5 0 .238-.006.387-.036.584-.118.785-.558 1.416-1.464 1.416-2.998 0-5.703-1.607-8.34-4.35-.136.414-.168.838-.164 1.635l.001.215c0 2.048.83 3.168 2.794 3.792l.698.222v.731c0 .887-.84 1.722-2.07 2.096.563.428 1.716.659 3.58.659h1v1c0 1.432-1.645 2.554-4.132 3.14.81.248 1.89.355 3.133.355 4.614 0 9.5-3.208 9.5-7.995v-2.5c0-.294.086-.55.227-.814.055-.104.116-.203.191-.316.015-.02.067-.098.109-.16l.08-.158.384-.77c-.2-.038-.395-.079-.586-.122Z"
+              fill="#DDD"
+            ></path>
+          </svg>
+        </div>
+      </a>
+      <a
+        href="https://in.pinterest.com/bigbasketcom/"
+        data-button="pinterest"
+        className="group"
+      >
+        <div className="absolute opacity-0 group-hover:opacity-100">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 4a4 4 0 0 1 4-4h16a4 4 0 0 1 4 4v16a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4Z"
+              fill="#fff"
+            ></path>
+            <path
+              d="M12.397 2C6.914 2 4 5.513 4 9.343c0 1.776.993 3.992 2.582 4.695.453.204.393-.045.783-1.537a.354.354 0 0 0-.085-.348c-2.272-2.627-.443-8.029 4.793-8.029 7.577 0 6.161 10.485 1.318 10.485-1.248 0-2.178-.98-1.884-2.192.356-1.444 1.055-2.997 1.055-4.038 0-2.623-3.909-2.234-3.909 1.242 0 1.074.38 1.8.38 1.8s-1.257 5.08-1.49 6.028c-.395 1.607.053 4.208.092 4.432.024.123.163.163.24.06.124-.162 1.644-2.33 2.07-3.897.155-.571.79-2.888.79-2.888.42.757 1.629 1.39 2.916 1.39 3.83 0 6.598-3.367 6.598-7.544C20.236 4.997 16.81 2 12.397 2Z"
+              fill="#000"
+            ></path>
+          </svg>
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-0">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.397 2C6.914 2 4 5.513 4 9.343c0 1.776.993 3.992 2.582 4.695.453.204.393-.045.783-1.537a.354.354 0 0 0-.085-.348c-2.272-2.627-.443-8.029 4.793-8.029 7.577 0 6.161 10.485 1.318 10.485-1.248 0-2.178-.98-1.884-2.192.356-1.444 1.055-2.997 1.055-4.038 0-2.623-3.909-2.234-3.909 1.242 0 1.074.38 1.8.38 1.8s-1.257 5.08-1.49 6.028c-.395 1.607.053 4.208.092 4.432.024.123.163.163.24.06.124-.162 1.644-2.33 2.07-3.897.155-.571.79-2.888.79-2.888.42.757 1.629 1.39 2.916 1.39 3.83 0 6.598-3.367 6.598-7.544C20.236 4.997 16.81 2 12.397 2Z"
+              fill="#DDD"
+            ></path>
+          </svg>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default Socials;
